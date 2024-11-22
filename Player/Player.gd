c7009cf9886a399d9ccc74642507e2ffcc9b04c3
Isaac RF <IsaacRF239@gmail.com>
2024-11-22 15:31:22 +0100

    Add death elements
    - Add hazard death area
    - Add player game over function, that triggers Death SFX and animation, and restarts the level
    - Add a camera follow component to allow stopping the camera on death

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-var move_speed : float = 4.0
+var move_speed : float = 6.0
 var acceleration : float = 0.3
 var decceleration : float = 0.4
 var rotation_speed : float = 0.1
@@ -10,6 +10,10 @@ var gravity : float = 20.0
 var facing_angle : float
 
 @onready var model = $Model
+@onready var death_sfx = $DeathSFX
+@onready var animation_player = $AnimationPlayer
+@onready var camera_follow = $CameraFollow
+@onready var camera = $Camera
 
 func _physics_process(delta):	
 	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
@@ -33,3 +37,10 @@ func _physics_process(delta):
 	if input.length() > 0 && is_on_floor():
 		facing_angle = Vector2(input.y, input.x).angle()
 		model.rotation.y = lerp_angle(model.rotation.y, facing_angle, rotation_speed)
+		
+func game_over():
+	animation_player.play("Death")
+	death_sfx.play()
+	camera_follow.update_position = false
+	await get_tree().create_timer(1.0).timeout
+	get_tree().reload_current_scene()
